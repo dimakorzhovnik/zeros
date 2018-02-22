0ede1de17274e719851b15e237be2cc6e5e50b9a
Dima <36644677+dimakorzhovnik@users.noreply.github.com>
2018-02-23 00:04:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+let zero = 0;
+let count = 5;
+while (number > count) {
+   zero = zero + Math.floor(number / count);
+   count = count * 5;
+  }
 }
